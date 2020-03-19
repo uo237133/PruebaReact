186ba57c9e8768d226f63711da65ae941dec882e
uo237133 <uo237133@uniovi.es>
2020-03-19 16:56:27 +0100

    Estilos

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,39 @@
 import React from "react";
+import "./Task.css";
 
 class Task extends React.Component {
+  styleCompleted() {
+    return {
+      fontSize: "20px",
+      color: this.props.tarea.done ? "gray" : "black",
+      textDecoration: this.props.tarea.done ? "line-through" : "none"
+    };
+  }
+
   render() {
     const { tarea } = this.props;
-
+    //Forma 3:const colorRed = { background: "red" };
     return (
-      <div>
+      // Forma 1: <div className="red"
+      // Forma 2: <div>style={{ background: "red" }}>
+      // Forma 3: <div style={colorRed}>
+      <div style={this.styleCompleted()}>
         {tarea.title} - {tarea.description} - {tarea.done} - {tarea.id}
         <input type="checkbox" />
-        <button>x</button>
+        <button style={btnDelete}>x</button>
       </div>
     );
   }
 }
+
+const btnDelete = {
+  fontSize: "18px",
+  background: "#ea2027",
+  color: "#fff",
+  border: "none",
+  padding: "10 px 15px",
+  borderRadius: "50%",
+  cursor: "pointer"
+};
 
 export default Task;
